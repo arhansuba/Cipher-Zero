0cf68f2f92cc3f8e80242df118ed7fda14cd728b
arhansuba <subasiarhan3@gmail.com>
2024-08-23 14:13:13 +0300

    commit 35

--- contracts/bitthetasecure/interfaces/ITheta.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
-
-interface ITheta {
-    function uploadVideo(string calldata videoHash) external;
-    function getVideos(address user) external view returns (string[] memory);
-}
--- frontend/src/theta/tns.js
+++
@@ -1,8 +0,0 @@
-import TNS from 'tns-resolver';
-import Config from '../config';
-
-const endpoint = Config.ethRPCEndpoint || "https://eth-rpc-api.thetatoken.org/rpc";
-
-const tns = new TNS({ customRpcEndpoint: endpoint });
-
-export default tns;
